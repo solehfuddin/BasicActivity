831bf67ff8e50275008ce17397a00318fca28bec
solehfuddin <solehfuddin@gmail.com>
2020-11-06 16:06:03 +0700

    Versi awal activity. 1. Intent 2. Siklus Aktivitas 3. Dialog Custom with XML

---
+++ app/src/main/java/com/example/basicactivity/TestDialog.kt
@@ -0,0 +1,35 @@
+package com.example.basicactivity
+
+import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+
+class TestDialog : DialogFragment() {
+    @SuppressLint("InflateParams")
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+//        return super.onCreateDialog(savedInstanceState)
+
+        return activity?.let {
+            val builder = AlertDialog.Builder(it)
+            // Get the layout inflater
+            val inflater = requireActivity().layoutInflater;
+
+            // Inflate and set the layout for the dialog
+            // Pass null as the parent view because its going in the dialog layout
+            builder.setView(inflater.inflate(R.layout.dialog_signin, null))
+                // Add action buttons
+                .setPositiveButton(R.string.signin,
+                    DialogInterface.OnClickListener { dialog, id ->
+                        // sign in the user ...
+                    })
+                .setNegativeButton(R.string.cancel,
+                    DialogInterface.OnClickListener { dialog, id ->
+                        getDialog()?.cancel()
+                    })
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+}
