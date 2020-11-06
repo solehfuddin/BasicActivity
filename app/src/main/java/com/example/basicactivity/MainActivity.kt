434597447b630122e7d61f7465ad2d7b443e1807
solehfuddin <solehfuddin@gmail.com>
2020-11-06 14:15:36 +0700

    Versi awal activity. 1. Intent 2. Siklus Aktivitas

---
+++ app/src/main/java/com/example/basicactivity/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.basicactivity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnKirim.setOnClickListener {
+            kirimPesan(txtPesan.text.toString())
+        }
+
+        Log.i("On Create", "Running On Create")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("On Start", "Running On Start")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("On Restart", "Running On Restart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("On Resume", "Running On Resume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("On Pause", "Running On Pause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("On Stop", "Running On Stop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("On Destroy", "Running On Destroy")
+    }
+
+    fun kirimPesan(isipesan : String) {
+        val pesan = Intent().apply {
+            action = Intent.ACTION_SEND
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TEXT, isipesan)
+        }
+        startActivity(pesan)
+    }
+}
---
+++ app/src/main/java/com/example/basicactivity/OutputActivity.kt
@@ -0,0 +1,11 @@
+package com.example.basicactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OutputActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_output)
+    }
+}
